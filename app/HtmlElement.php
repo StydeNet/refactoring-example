3c4095bf7addf0f2eeb8bbdda07b15b87976cdd7
Duilio Palacios <duilio@styde.net>
2017-11-29 13:49:08 +0000

    First commit

---
+++ app/HtmlElement.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class HtmlElement
+{
+    public function render()
+    {
+        return 'HTML ELEMENT HERE';
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$element = new \App\HtmlElement;
+
+echo $element->render();
