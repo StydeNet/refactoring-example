a3998a63ef291bd1902fc94409c24c71fa9c112a
Duilio Palacios <duilio@styde.net>
2017-12-01 12:47:39 +0000

    Install and configure PHPUnit

---
+++ tests/HtmlElementTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests;
+
+class HtmlElementTest extends TestCase
+{
+    /** @test */
+    function it_generates_html_elements()
+    {
+        $this->markTestIncomplete();
+    }
+}
