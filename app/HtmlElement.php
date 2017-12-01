f3ece36066de48a1ef6d1eef7ff8dd348232715f
Duilio Palacios <duilio@styde.net>
2017-12-01 11:18:09 +0000

    Add functionality to generate HTML dinamically with examples

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -4,8 +4,57 @@ namespace App;
 
 class HtmlElement
 {
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var null
+     */
+    private $content;
+    /**
+     * @var array
+     */
+    private $attributes;
+
+    public function __construct(string $name, array $attributes = [], $content = null)
+    {
+        $this->name = $name;
+        $this->attributes = $attributes;
+        $this->content = $content;
+    }
+    
     public function render()
     {
-        return 'HTML ELEMENT HERE';
+        // Si el elemento tiene atributos:
+        if (! empty($this->attributes)) {
+            $htmlAttributes = '';
+
+            foreach ($this->attributes as $attribute => $value) {
+                if (is_numeric($attribute)) {
+                    $htmlAttributes .= ' '.$value;
+                } else {
+                    $htmlAttributes .= ' '.$attribute.'="'.htmlentities($value, ENT_QUOTES, 'UTF-8').'"'; // name="value"
+                }
+            }
+
+            // Abrir la etiqueta con atributos
+            $result = '<'.$this->name.$htmlAttributes.'>';
+        } else {
+            // Abrir la etiqueta sin atributos
+            $result = '<'.$this->name.'>';
+        }
+
+        // Si el elemento es void
+        if (in_array($this->name, ['br', 'hr', 'img', 'input', 'meta'])) {
+            return $result;
+        }
+
+        // Imprimir el contenido
+        $result .= htmlentities($this->content, ENT_QUOTES, 'UTF-8');
+        // Cerrar la etiqueta
+        $result .= '</'.$this->name.'>';
+
+        return $result;
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,6 +2,36 @@
 
 require '../vendor/autoload.php';
 
-$element = new \App\HtmlElement;
+$element = new \App\HtmlElement('p', [], 'Este es el contenido');
 
-echo $element->render();
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('p', ['id' => 'my_paragraph'], 'Este es el contenido');
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('p', ['id' => 'my_paragraph', 'class' => 'paragraph'], 'Este es el contenido');
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('img', ['src' => 'img/styde.png']);
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('img', ['src' => 'img/styde.png', 'title' => 'Curso de "Refactorización" en Styde']);
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('input', ['required']);
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
