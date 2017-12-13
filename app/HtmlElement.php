5cd86ec0813d7bdc4d20fe56facfe3e9bebfc3ae
Duilio Palacios <duilio@styde.net>
2017-12-13 18:12:33 +0000

    Delete temporary variables

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -26,17 +26,11 @@ class HtmlElement
     
     public function render()
     {
-        $result = $this->open();
-
         if ($this->isVoid()) {
-            return $result;
+            return $this->open();
         }
 
-        $result .= $this->content();
-
-        $result .= $this->close();
-
-        return $result;
+        return $this->open().$this->content().$this->close();
     }
 
     public function open(): string
@@ -46,13 +40,11 @@ class HtmlElement
 
     public function attributes(): string
     {
-        $htmlAttributes = '';
+        // $htmlAttributes
 
-        foreach ($this->attributes as $attribute => $value) {
-            $htmlAttributes .= $this->renderAttribute($attribute, $value);
-        }
-
-        return $htmlAttributes;
+        return array_reduce(array_keys($this->attributes), function ($result, $attribute) {
+            return $result . $this->renderAttribute($attribute);
+        }, '');
     }
 
     public function hasAttributes(): bool
@@ -60,13 +52,13 @@ class HtmlElement
         return ! empty($this->attributes);
     }
 
-    protected function renderAttribute($attribute, $value)
+    protected function renderAttribute($attribute)
     {
         if (is_numeric($attribute)) {
-            return ' '.$value;
+            return ' '.$this->attributes[$attribute];
         }
 
-        return ' '.$attribute.'="'.htmlentities($value, ENT_QUOTES, 'UTF-8').'"'; // name="value"
+        return ' '.$attribute.'="'.htmlentities($this->attributes[$attribute], ENT_QUOTES, 'UTF-8').'"'; // name="value"
     }
 
     public function isVoid(): bool
