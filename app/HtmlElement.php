81ac2903c15136dabb0ef1ad7ee0d00499bb10ae
Duilio Palacios <duilio@styde.net>
2017-12-14 23:26:19 +0000

    Add HtmlAttributes class

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -20,7 +20,7 @@ class HtmlElement
     public function __construct(string $name, array $attributes = [], $content = null)
     {
         $this->name = $name;
-        $this->attributes = $attributes;
+        $this->attributes = new HtmlAttributes($attributes);
         $this->content = $content;
     }
     
@@ -40,25 +40,7 @@ class HtmlElement
 
     public function attributes(): string
     {
-        // $htmlAttributes
-
-        return array_reduce(array_keys($this->attributes), function ($result, $attribute) {
-            return $result . $this->renderAttribute($attribute);
-        }, '');
-    }
-
-    public function hasAttributes(): bool
-    {
-        return ! empty($this->attributes);
-    }
-
-    protected function renderAttribute($attribute)
-    {
-        if (is_numeric($attribute)) {
-            return ' '.$this->attributes[$attribute];
-        }
-
-        return ' '.$attribute.'="'.htmlentities($this->attributes[$attribute], ENT_QUOTES, 'UTF-8').'"'; // name="value"
+        return $this->attributes->render();
     }
 
     public function isVoid(): bool
