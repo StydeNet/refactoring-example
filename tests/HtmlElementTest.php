b19090b0aa98e92ca8aaa1b0667493aaf2a06e28
Simon Montoya Palacio <simon.montoya@ubiquom.com>
2019-05-10 08:48:27 -0500

    discard

--- tests/HtmlElementTest.php
+++ tests/HtmlElementTest.php
@@ -1,88 +1,81 @@
 <?php
 
+
 namespace Tests;
+
 
 use App\HtmlElement;
 
 class HtmlElementTest extends TestCase
 {
-    /** @test */
-    function it_checks_if_a_element_is_void_or_not()
+    function test_it_generates_a_paragraph_with_content()
     {
-        $this->assertFalse((new HtmlElement('p'))->isVoid());
-
-        $this->assertTrue((new HtmlElement('img'))->isVoid());
-    }
-    
-    /** @test */
-    function it_generates_attributes()
-    {
-        $element = new HtmlElement('span', ['class' => 'a_span', 'id' => 'the_span']);
-
-        $this->assertSame(' class="a_span" id="the_span"', $element->attributes());
-    }
-
-    /** @test */
-    function it_generates_a_paragraph_with_content()
-    {
-        $element = new HtmlElement('p', [], 'Este es el contenido');
+        $element = new HtmlElement('p',[],'este es el contenido');
 
         $this->assertSame(
-            '<p>Este es el contenido</p>',
+            '<p>este es el contenido</p>',
             $element->render()
         );
     }
 
-    /** @test */
-    function it_generates_a_paragraph_with_content_and_an_id_attribute()
+    function test_it_generates_a_paragraph_with_content_and_once_attribute()
     {
-        $element = new HtmlElement(
-            'p', ['id' => 'my_paragraph'], 'Este es el contenido'
-        );
+        $element = new HtmlElement('p',['id' => 'my_paragraph' ],'este es el contenido');
 
         $this->assertSame(
-            '<p id="my_paragraph">Este es el contenido</p>',
+            '<p id="my_paragraph">este es el contenido</p>',
             $element->render()
         );
     }
 
-    /** @test */
-    function it_generates_a_paragraph_with_multiple_attributes()
+    function test_it_generates_a_paragraph_with_content_and_twice_attribute()
     {
-        $element = new HtmlElement(
-            'p', ['id' => 'my_paragraph', 'class' => 'paragraph'], 'Este es el contenido'
-        );
+        $element = new HtmlElement('p',
+            ['id' => 'my_paragraph','class' => 'paragraph' ],'este es el contenido');
 
         $this->assertSame(
-            '<p id="my_paragraph" class="paragraph">Este es el contenido</p>',
+            '<p id="my_paragraph" class="paragraph">este es el contenido</p>',
             $element->render()
         );
     }
 
-    /** @test */
-    function it_generates_an_img_tag()
+    function test_it_generates_a_tag_image()
     {
-        $element = new HtmlElement('img', ['src' => 'img/styde.png']);
-
-        $this->assertSame('<img src="img/styde.png">', $element->render());
-    }
-
-    /** @test */
-    function it_escapes_the_html_attributes()
-    {
-        $element = new HtmlElement('img', ['src' => 'img/styde.png', 'title' => 'Curso de "Refactorización" en Styde']);
+        $element = new HtmlElement('img',['src' => 'img/img.png']);
 
         $this->assertSame(
-            '<img src="img/styde.png" title="Curso de &quot;Refactorizaci&oacute;n&quot; en Styde">',
+            '<img src="img/img.png">',
             $element->render()
         );
     }
 
-    /** @test */
-    function it_generates_elements_with_boolean_attributes()
+    function test_it_generates_a_tag_image_it_escapes_the_html_attributes()
     {
-        $element = new HtmlElement('input', ['required']);
+        $element = new HtmlElement('img',
+            ['src' => 'img/img.png','title' => 'Curso de "Refactorizacion" en styde']);
 
-        $this->assertSame('<input required>', $element->render());
+        $this->assertSame(
+            '<img src="img/img.png" title="Curso de &quot;Refactorizacion&quot; en styde">',
+            $element->render()
+        );
     }
+
+    function test_it_generates_a_tag_input_with_boolean_attributes()
+    {
+        $element = new HtmlElement('input',['required']);
+
+        $this->assertSame(
+            '<input required>',
+            $element->render()
+        );
+    }
+
+    function test_it_generates_a_tag_input_without_boolean_attributes()
+    {
+        $element = new HtmlElement('input');
+
+        $this->assertSame(
+            '<input>',
+            $element->render()
+
 }
